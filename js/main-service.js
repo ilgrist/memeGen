20b9bc719d0a3a49ca3807f24f86aa59386d729d
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-06-09 13:02:37 +0300

    added basic line draw

--- js/main-service.js
+++ js/main-service.js
@@ -1,25 +1,5 @@
 var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['happy'] }];
-var gMeme = {
-  selectedImgId: 1,
-  selectedLineIdx: 0,
-  lines: [
-    {
-      txt: 'I never eat Falafel',
-      size: 20,
-      align: 'left',
-      color: 'red',
-    },
-  ],
-};
 
-function _getCurrMeme() {
-  return gMeme;
-}
-
-function _getCurrImgURL() {
-  var imgId = gMeme.selectedImgId;
-  var img = gImgs.find((image) => {
-    return image.id === imgId;
-  });
-  return img.url;
+function _loadFont() {
+  document.querySelector('.canvas-cont').style.fontFamily = 'impact';
 }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,71 @@
 var gCanvas;
 var gCtx;
+var gMeme = {
+  selectedImgId: 1,
+  selectedLineIdx: 0,
+  lines: [
+    {
+      txt: 'Fly, you fools!',
+      size: 40,
+      align: 'center',
+      color: 'white',
+    },
+  ],
+};
 
 function onInit() {
   console.log('init!');
   gCanvas = document.querySelector('canvas');
   gCtx = gCanvas.getContext('2d');
   drawMemeImg();
+  _loadFont();
 }
 
 function onCanvasClick(ev) {}
 
 function drawMemeImg() {
-  var memeImg = _getCurrImgURL();
+  var memeImg = _getImgURL();
   var img = new Image();
   img.src = memeImg;
   img.onload = () => {
     gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
   };
+}
+
+function drawLine(lineIdx) {
+  //   drawCenterLine();
+  const line = _getMemeLine(lineIdx);
+  const text = line.txt;
+  gCtx.lineWidth = 2;
+  gCtx.fillStyle = line.color;
+  //   gCtx.font = '40px impact';
+  gCtx.font = `${line.size}px ` + 'impact';
+  gCtx.textAlign = line.align;
+  const x = 225;
+  const y = 50;
+  gCtx.fillText(text, x, y);
+  gCtx.strokeText(text, x, y);
+}
+
+function _getMeme() {
+  return gMeme;
+}
+
+function _getImgURL() {
+  var imgId = gMeme.selectedImgId;
+  var img = gImgs.find((image) => {
+    return image.id === imgId;
+  });
+  return img.url;
+}
+
+function _getMemeLine(lineIdx) {
+  return gMeme.lines[lineIdx];
+}
+
+function drawCenterLine() {
+  gCtx.strokeStyle = 'red';
+  gCtx.moveTo(225, 20);
+  gCtx.lineTo(225, 170);
+  gCtx.stroke();
 }
