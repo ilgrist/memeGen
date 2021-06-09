1526d55f3ac3551d9692fdde2ce14a78f9465a73
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-06-09 11:31:19 +0300

    added canvas+one img

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+var gCanvas;
+var gCtx;
+
 function onInit() {
   console.log('init!');
+  gCanvas = document.querySelector('canvas');
+  gCtx = gCanvas.getContext('2d');
+  drawMemeImg();
+}
+
+function onCanvasClick(ev) {}
+
+function drawMemeImg() {
+  var memeImg = _getCurrImgURL();
+  var img = new Image();
+  img.src = memeImg;
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+  };
 }
